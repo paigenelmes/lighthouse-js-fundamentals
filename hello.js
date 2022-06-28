9b27d18451125f0dac6e90ff758b7c656dec77ce
Paige Nelmes <designer@paigenelmes.com>
2022-06-28 09:53:31 -0700

    Hello Functions Exercise

--- hello.js
+++ hello.js
@@ -5,3 +5,20 @@ const sayHello = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+//using console.log in a function
+
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+
+sayHelloToConsole('John');
+
+//using a return statement in a function 
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+
+const greeting = returnSayHello('John');
+console.log(greeting);
