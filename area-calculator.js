8afd94426a5be8e39c6b5e0c8dd4c738232bc0ef
Paige Nelmes <designer@paigenelmes.com>
2022-06-28 15:33:41 -0700

    Area calculator exercise

---
+++ area-calculator.js
@@ -0,0 +1,25 @@
+function calculateRectangleArea(length, width) {
+  if (length >= 0 && width >= 0){
+    return (length * width);
+  }else{
+    return undefined
+  }
+}
+function calculateTriangleArea(base, height) {
+  if (base >= 0 && height >= 0){
+    return (base * height / 2);
+  }else{
+    return undefined
+  }
+}
+function calculateCircleArea(radius) {
+  if (radius >= 0) {
+    return Math.PI * (radius * radius);
+  }else{
+    return undefined
+  }
+}
+
+console.log(calculateRectangleArea(10, 5));
+console.log(calculateTriangleArea(10, 5));
+console.log(calculateCircleArea(10));
