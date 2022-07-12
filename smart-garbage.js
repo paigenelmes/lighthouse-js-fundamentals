b6390c7a5b2ed994be9e21fd89720898ad861eb2
Paige Nelmes <designer@paigenelmes.com>
2022-07-12 10:56:29 -0700

    Smart Garbage Exercise

---
+++ smart-garbage.js
@@ -0,0 +1,13 @@
+function smartGarbage(trash, bins) {
+  if (trash === "recycling") {
+    bins["recycling"]++;
+  } else if (trash === "waste") {
+    bins["waste"]++;
+  } else if (trash === "compost") {
+    bins["compost"]++;
+  } return bins;
+}  
+
+console.log(smartGarbage("recycling", { waste: 4, recycling: 2, compost: 5 }));
+
+
